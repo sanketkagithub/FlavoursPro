0f2389237c53a2d3f481e309f6117df885a353cc
Sanket <sanket.kpatankar@cytrion.com>
2018-10-17 14:58:43 +0530

    ffgfhfghgff

--- app/src/main/java/testapp/testing/com/flavourspro/MainActivity.java
+++ app/src/main/java/testapp/testing/com/flavourspro/MainActivity.java
@@ -2,12 +2,28 @@ package testapp.testing.com.flavourspro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import testpaid.testing.com.flavourspro.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+       /* if (BuildConfig.BASE_MAIN_URL.equals("http://www.givmebikeFree.com"))
+        {*/
+            setContentView(R.layout.activity_main);
+
+       /* }else if (BuildConfig.BASE_MAIN_URL.equals("http://www.givmebikePaid.com"))
+        {
+            setContentView(R.layout.activity_mainpaid);
+
+        }*/
+
+
+
     }
 }
---
+++ app/src/paid/java/testpaid/testing/com/flavours/MainPaidActivity.java
@@ -0,0 +1,16 @@
+package testpaid.testing.com.flavours;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import testpaid.testing.com.flavourspro.R;
+
+
+public class MainPaidActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_paid);
+    }
+}
